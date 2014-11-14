496d89322d18fadcaccd1036ea4a295ec73fa10e
Emanuele DelBono <emanuele@codiceplastico.com>
2014-11-14 14:53:28 +0100

    added log information

--- index.js
+++ index.js
@@ -4,19 +4,26 @@ var http = require('http'),
     util = require('util'),
     async = require('async'),
     AdmZip = require('adm-zip'),
+    winston = require('winston'),
     exec = require('child_process').exec;
 
+
+winston.add(winston.transports.File, { filename: 'installs.log' });
+winston.remove(winston.transports.Console);
 
 process.env.TMPDIR ||= '/vsp/temp';
 var workingFolder = './unzipped';
 
 http.createServer(function(req, res) {
   if (req.method === 'POST') {
+    winston.log('____________________________________________________________');
+    winston.log('New install: %s', new Date().toString());
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files) {
       res.writeHead(200, {'content-type': 'text/plain'});
       res.write('received upload:\n\n');
       res.write(util.inspect({fields: fields, files: files}));
+      winston.log('files: %s', files);
     });
 
     form.on('end', function() {
@@ -27,19 +34,28 @@ http.createServer(function(req, res) {
       var zip = new AdmZip(fileName);
       zip.extractAllTo(workingFolder, true);
 
+      winston.log('file extracted');
       var spec = require(workingFolder + '/spec.json');
         
+      winston.log('spec file: %s', spec.name);
+      winston.log('spec version: %s', spec.version);
+
       if (spec.commands.length === 0){
         res.writeHead(200, {'Content-Type': 'text/plain'});
+        winston.log('no commands to execute');
         res.end('nothing to do');
       }
         
       res.writeHead(200, {'Content-Type': 'text/plain'});
-          
+      
+       winston.log('%d commands to execute', cmd.length);
+
       var cmds = spec.commands.map(function(cmd){
         return function(acallback){
+          winston.log('execute command: %s', cmd);
           exec(cmd, {cwd: workingFolder}, function callback(error, stdout, stderr){
           if (error){
+            winston.error('command: %s %s %s', cmd, error, stderr);
             res.write('ERRORS');
             res.write(error.toString());
             res.write(stderr.toString());
@@ -47,13 +63,14 @@ http.createServer(function(req, res) {
             acallback();
             return;
           }
-          res.write(stdout||'.');
+          res.write(stdout || '.');
           acallback();
           });
        }
       });
 
       async.series(cmds, function(){
+        winston.log('completed');
         res.end('\nCompleted');
       });
 
@@ -69,5 +86,6 @@ http.createServer(function(req, res) {
   res.writeHead(404);
   res.end();
 }).listen(process.env.PORT, function() {
+  winston.log('server listening');
   console.log('Listening...');
 });
